f0793e37ebbc3f10e83937ba2ba2a65094134a13
Eti-Kumawat <eti.kumawat@aeonx.digital>
2023-05-29 12:34:39 +0530

    initial commit

---
+++ Demo.js
@@ -0,0 +1,60 @@
+// let day = prompt("What's your sign?");
+// switch (day) {
+//     case "monday":
+//         console.log('monday...........................');
+//         break;
+//     case "tuesday":
+//         console.log('tuesday...........................');
+//         break;
+//     case "wedesday":
+//         console.log('wedesdayyy...........................');
+//         break;
+
+//     default:
+//         break;
+// }
+
+// let student="eti"
+// for(let i of student){
+// console.log(i);
+// }
+
+// let arr = [2, 4, 5, 1, 8, 7]
+// let a=arr.reduce((a1,a2) => { return a1+a2 }
+// )
+// console.log(a);
+// console.log(arr); 
+
+// let arr1 = [2, 4, 5, 1, 8, 7]
+// arr1.map((value, index, array) =>
+//     console.log(value, index, array)
+// )
+
+// let arr = []
+// let a=prompt("Enter a number")
+// for(i=0;i<=arr;i++){
+//     arr.push(a)
+// }
+// let qb = [1, 2, 3, 4]
+
+// let ans = qb.reduce((x, y, z) => {
+//     return x * y * z;
+// })
+// console.log(ans);
+
+// let a = prompt("Enter Number");
+// let b = Math.random(a)
+// for (let i = 0; a < 10 && a > 5; i++) {
+//     console.log(a);
+// }
+// console.log('hey');
+// console.info('hey')
+// console.warn('danger')
+
+console.time("test");
+let i = 0;
+while (i < 5) {
+    console.log("", i);
+    i++;
+}
+console.timeEnd("test");
